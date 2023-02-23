b38cc73e28e9885fb6b9248bc966ab8aae8c2f77
Aisem13 <36743569+Aisem13@users.noreply.github.com>
2023-02-23 13:27:03 +0000

    Add Todo component

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -13,14 +13,12 @@
     </section>
 
     <section>
-      <div v-for="todo in todos" class="todo" :key="todo.id">
-        <p>{{ todo.title }}</p>
-        <div>
-          <button @click="removeTodo(todo)" class="remove-todo-btn">
-            &times;
-          </button>
-        </div>
-      </div>
+      <Todo
+        v-for="todo in todos"
+        :title="todo.title"
+        :key="todo.id"
+        @remove="removeTodo(todo.id)"
+      />
     </section>
   </main>
 </template>
@@ -29,6 +27,7 @@
 import AddTodoForm from "./components/AddTodoForm.vue";
 import Alert from "./components/Alert.vue";
 import Navbar from "./components/Navbar.vue";
+import Todo from "./components/Todo.vue";
 
 export default {
   data() {
@@ -49,33 +48,10 @@ export default {
         id: Math.floor(Math.random() * 1000),
       });
     },
-    removeTodo(todoTitle) {
-      this.todos = this.todos.filter((todo) => todo !== todoTitle);
+    removeTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
     },
   },
-  components: {Alert, Navbar, AddTodoForm},
+  components: {Alert, Navbar, AddTodoForm, Todo},
 };
 </script>
-
-<style scoped>
-.todo {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: var(--accent-color);
-  margin-top: 30px;
-  padding: 0 20px 0 20px;
-  border-radius: 10px;
-}
-
-.remove-todo-btn {
-  border-radius: 50%;
-  border: none;
-  height: 40px;
-  width: 40px;
-  font-size: 30px;
-  color: var(--text-color);
-  background: var(--danger-color);
-  cursor: pointer;
-}
-</style>
